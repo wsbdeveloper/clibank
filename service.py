581f50ec6f2d4bf779f5745ed44499e2dabe543e
w3x7 <w3x7@kali>
2025-09-03 20:20:47 -0300

    feat: start the service and prototype the code

---
+++ service.py
@@ -0,0 +1,20 @@
+def IExecutionService:
+    def _process_buy(self, operation: Operation) -> str:
+        pass
+
+    def _process_sell(self, operation: Operation) -> str:
+        pass
+
+class ExecutionService(IExecutionService):
+    """
+        TODO: realizar processamento de operações dado o requerimento do pdf
+    """
+    def _process_buy(self, operation: Operation) -> str:
+        pass
+
+    def _process_sell(self, operation: Operation) -> str:
+        pass
+
+
+
+

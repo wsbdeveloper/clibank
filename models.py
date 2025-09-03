a02c947f73bddd0cf3c03cf4b5534a7c356c840f
Macbook <macbook@Wellingtons-MacBook-Pro.local>
2025-09-02 23:02:47 -0300

    feat: add new class for models class

---
+++ models.py
@@ -0,0 +1,8 @@
+class Operation:
+    """ Model the operations the transaction"""
+
+    def __init__(self, operation: str, unit_cost: str, quantity: str):
+        self.operation = operation
+        self.unit_cost = unit_cost
+        self.quantity = quantity
+

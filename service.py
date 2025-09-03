461c315d1c6a8c5c950175486ae72b6bf6a642b4
w3x7 <w3x7@kali>
2025-09-03 20:33:00 -0300

    feat: withted average price function rules

--- service.py
+++ service.py
@@ -1,4 +1,6 @@
-def IExecutionService:
+from decimal import Decimal
+
+def IExecutionService():
     def _process_buy(self, operation: Operation) -> str:
         pass
 
@@ -9,9 +11,22 @@ class ExecutionService(IExecutionService):
     """
         TODO: realizar processamento de operações dado o requerimento do pdf
     """
-    def _process_buy(self, operation: Operation) -> str:
-        pass
 
+    def __init__(self):
+        self.total_shares = 0
+        self.weighted_average_price = Decimal('0.0')
+        self.accumulated_loss = Decimal('0.0')
+
+    def _process_buy(self, operation: Operation) -> str:
+        """ Process a buy operation"""
+        if self.total_shares == 0: # first buy
+            self.weighted_average_price = operation.unit_cost
+        else:
+            total_cost = self.total_shares * self.weighted_average_price
+            new_cost = operation.quantity * operation.unit_cost
+            self.weighted_average_price = (total_cost + new_cost) / (self.total_shares + operation.quantity)
+        return "0.00"
+        
     def _process_sell(self, operation: Operation) -> str:
         pass
 
